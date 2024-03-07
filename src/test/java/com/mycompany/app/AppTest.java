8e8dfc69508e37876562aff2defbd3f7b96f0ddb
tunak <tunakodal43@gmail.com>
2024-03-07 14:59:12 +0300

    Getting ready for deployment to heroku

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,6 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Unit test for simple App.
  */
@@ -35,4 +38,25 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @org.junit.Test
+    public void testFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertTrue(new App().search(array, 4));
+    }
+    @org.junit.Test
+    public void tstNotFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertFalse(new App().search(array, 5));
+    }
+    @org.junit.Test
+    public void testEmptyArray() {
+        ArrayList<Integer> array = new ArrayList<>();
+        assertFalse(new App().search(array, 1));
+    }
+    @org.junit.Test
+    public void testNull() {
+        assertFalse(new App().search(null, 1));
+    }
+
 }
